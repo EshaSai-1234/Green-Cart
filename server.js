30fec3915da811c900234274fcd3457d85181454
Esha Sai <eshasai709@gmail.com>
2024-10-17 20:04:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+ const express = require('express');
+ const mongoose = require('mongoose');
+ const Product = require('./models/Product');
+ const app = express();
+ const PORT = process.env.PORT || 3001;
+ mongoose.connect('mongodb://localhost:27017/greencart', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ });
+ app.get('/api/products', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+ });
+ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
